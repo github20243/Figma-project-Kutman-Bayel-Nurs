9fd320a22e5946fb86a812c3dbedb4255a4c3494
nurs_996 <98372q@gmail.com>
2024-07-05 15:07:19 +0600

    было не большой изминение

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import {styled} from '@mui/material'
-import './App.css'
-import Cardswithdescription from './components/Cardswithdescription'
-import Info from './components/Info'
-import Info2 from './components/Info2'
+import { styled } from "@mui/material";
+import "./App.css";
+import Cardswithdescription from "./components/Cardswithdescription";
+import Info from "./components/Info";
+import Info2 from "./components/Info2";
 
 function App() {
-
-  return (
-    <>
-     <StyledContoinet>
-   {/* <Cardswithdescription /> */}
-      <Info/>
-      <Info2/>
-     </StyledContoinet> 
-    </>
-  )
+	return (
+		<>
+			<StyledContoinet>
+				{/* <Cardswithdescription /> */}
+				<Info />
+        <Info2/>
+			</StyledContoinet>
+		</>
+	);
 }
 
-export default App
+export default App;
 
 const StyledContoinet = styled("div")`
-  width: 1519px;
-  min-height: 600px;
+	width: 1519px;
+	min-height: 600px;
   margin: 0 auto;
-`
-
-
+`;
